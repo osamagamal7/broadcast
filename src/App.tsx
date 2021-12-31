cd1afb2b5bd13c0a54a77286cae18537da0f0aca
Osama Gamal <osamagamal345@gmail.com>
2022-01-01 01:01:10 +0200

    configure graphql with apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {ApolloProvider} from '@apollo/client';
 
 import {AppNavContainer} from './navigations';
+import {client} from './graphql/client';
 
 export const App: React.FC = () => {
-  return <AppNavContainer />;
+  return (
+    <ApolloProvider client={client}>
+      <AppNavContainer />
+    </ApolloProvider>
+  );
 };
