735d957fa824afca16edb84790651630fe76ca34
Osama Gamal <osamagamal345@gmail.com>
2022-01-14 00:55:58 +0200

    configuring RN-track-player

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import {App} from './src/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() =>
+  require('./src/utilsAndServices/trackPlayerServices'),
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {ApolloProvider} from '@apollo/client';
+import {ActivityIndicator} from 'react-native';
 
 import {AppNavContainer} from './navigations';
 import {client} from './graphql/client';
+import {theme} from './assets/theme/colors';
+import {PlayerProvider} from './context/PlayerProvider';
+import TrackPlayer from 'react-native-track-player';
 
 export const App: React.FC = () => {
+  const [isReady, setIsReady] = React.useState<boolean>(false);
+
+  const setup = async () => {
+    await TrackPlayer.setupPlayer();
+    setIsReady(true);
+  };
+
+  React.useEffect(() => {
+    setup();
+  }, []);
+
   return (
     <ApolloProvider client={client}>
-      <AppNavContainer />
+      {isReady ? (
+        <PlayerProvider>
+          <AppNavContainer />
+        </PlayerProvider>
+      ) : (
+        <ActivityIndicator
+          size="large"
+          color={theme.colorBlueLight}
+          style={{justifyContent: 'center', alignItems: 'center', flex: 1}}
+        />
+      )}
     </ApolloProvider>
   );
 };
