cf4997051c0ed9be4926fcaa8caa46ac2286711c
Osama Gamal <osamagamal345@gmail.com>
2022-01-20 18:17:47 +0200

    list the subscribed items in stored in DB

--- src/screens/Library/index.tsx
+++ src/screens/Library/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {theme} from '../../assets/theme/colors';
+import {useDBContext} from '../../context/DBContext';
 
 export const Library: React.FC = () => {
+  const {broadcasts} = useDBContext();
   return (
-    <View>
-      <Text>Library Screen</Text>
+    <View style={{flex: 1}}>
+      {broadcasts.map(item => (
+        <View
+          key={item.feedUrl}
+          style={{
+            backgroundColor: theme.colorWhite,
+            padding: 20,
+            marginVertical: 10,
+          }}>
+          <Text>{item.name}</Text>
+        </View>
+      ))}
     </View>
   );
 };
